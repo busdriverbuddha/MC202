892f9cacd9443f79664f632a393f244e5ac77623
Guilherme Gama <guilhermegama@gmail.com>
2016-08-06 18:00:21 -0300

    06/08/2016

--- Sedgewick/ch3/ex1.c
+++ Sedgewick/ch3/ex1.c
@@ -2,22 +2,34 @@
 #include <stdlib.h>
 #include "gcd.h"
 
-#define MAX 10
+#define MAX 50
 
 int main (int argc, char const *argv[])
 {
 	int a[MAX][MAX];
-	int i, j;
+	int i, j, g;
 	for (i = 1; i < MAX; i++)
 	{
-		printf("i = %d j = %d\n", i, j);
 		for (j = 1; j < MAX; j++)
 		{
-			if (gcd(i, j) == 1)
+			g = gcd(i, j);
+			if (g == 1)
 				a[i][j] = 1;
 			else
 				a[i][j] = 0;
 		}
+	}
+	for (i = 1; i < MAX; i++)
+	{
+		printf("[%d]\t", i);
+		for (j = 1; j < MAX; j++)
+		{
+			if (a[i][j] != 0)
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
 	}
 	return 0;
 }
--- Sedgewick/ch3/gcd.c
+++ Sedgewick/ch3/gcd.c
@@ -12,8 +12,6 @@ int gcd(int u, int v)
 			v = t;
 		}
 		u = u - v;
-		v = u;
-		//printf("u = %d v = %d\n", u, v);
 	}
 	
 	return v;
